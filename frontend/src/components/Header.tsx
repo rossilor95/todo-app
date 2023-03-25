5626ab6235370b44a963c23020554c328abd2af9
rossilor95 <rossilor95@outlook.it>
2023-03-26 00:06:19 +0100

    chore(frontend): uniform style

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,8 +1,8 @@
-import { AppBar, Box, Toolbar, Typography } from '@mui/material';
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
 
 export default function Header() {
   return (
-    <Box sx={{ flexGrow: 1, position: 'sticky' }}>
+    <Box sx={{ flexGrow: 1, position: "sticky" }}>
       <AppBar>
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
