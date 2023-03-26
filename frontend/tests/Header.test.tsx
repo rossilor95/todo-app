5e5da90fba7db1a8fc02c200a0371b3160e8670c
rossilor95 <rossilor95@outlook.it>
2023-03-26 15:54:03 +0200

    chore(frontend): move tests to a separate dir

--- frontend/tests/Header.test.tsx
+++ frontend/tests/Header.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
-import Header from "./Header";
+import Header from "../src/components/Header";
 
 describe("Header", () => {
   it("should render the app title", () => {
