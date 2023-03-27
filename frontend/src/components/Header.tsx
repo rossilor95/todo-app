61df68dd625616e173d43005edcca676adf68da1
rossilor95 <rossilor95@outlook.it>
2023-03-27 22:30:53 +0200

    chore(frontend): adjust Header styling

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,9 @@
 export default function Header() {
   return (
-    <div className="sticky bg-blue-500">
-        <div className="container mx-6 py-3">
-          <h1 className="text-white text-xl font-medium text-left">Todo App</h1>
-        </div>
+    <div className="sticky bg-blue-600">
+      <div className="container mx-6 py-3">
+        <h1 className="text-white text-xl font-medium">Todo App</h1>
+      </div>
     </div>
   );
 }
