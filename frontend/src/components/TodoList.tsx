2a784f633339bd3659d67cff914ad2185fe8c3f4
rossilor95 <rossilor95@outlook.it>
2023-03-27 23:51:11 +0200

    feat(frontend): add TodoList component

---
+++ frontend/src/components/TodoList.tsx
@@ -0,0 +1,46 @@
+import { Todo } from "../interfaces";
+
+interface TodoItemProps {
+  todo: Todo;
+  handleToggleTodo: (id: number) => void;
+}
+
+interface TodoListProps {
+  todos: Todo[];
+  handleToggleTodo: (id: number) => void;
+}
+
+function TodoItem({ todo, handleToggleTodo }: TodoItemProps) {
+  return (
+    <li className="bg-white border-b border-gray-200 shadow hover:bg-blue-50 sm:rounded-md mb-4">
+      <div className="px-4 py-4 sm:px-6">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center">
+            <input
+              id={`todo-${todo.id}`}
+              type="checkbox"
+              className="h-4 w-4 rounded-full checked:bg-blue-600 hover:border-blue-600"
+              checked={todo.isCompleted}
+              onChange={() => handleToggleTodo(todo.id)}
+            />
+            <label htmlFor={`todo-${todo.id}`} className="ml-3">
+              <p className={todo.isCompleted ? "line-through text-gray-500" : undefined}>
+                {todo.text}
+              </p>
+            </label>
+          </div>
+        </div>
+      </div>
+    </li>
+  );
+}
+
+export default function TodoList({ todos, handleToggleTodo }: TodoListProps) {
+  return (
+    <ul className="mx-6">
+      {todos.map((todo) => (
+        <TodoItem todo={todo} handleToggleTodo={handleToggleTodo} />
+      ))}
+    </ul>
+  );
+}
