0adb832c03876d602827c3fef75b6cd3594877d9
rossilor95 <rossilor95@outlook.it>
2023-03-29 16:08:51 +0200

    chore(frontend): simplify TodoList

--- frontend/src/components/TodoList.tsx
+++ frontend/src/components/TodoList.tsx
@@ -15,23 +15,19 @@ interface TodoListProps {
 function TodoItem({ todo, handleToggleTodo }: TodoItemProps) {
   return (
     <li className="bg-white border-b border-gray-200 shadow hover:bg-blue-50 sm:rounded-md mb-4">
-      <div className="px-4 py-4 sm:px-6">
-        <div className="flex items-center justify-between">
-          <div className="flex items-center">
-            <input
-              id={`todo-${todo.id}`}
-              type="checkbox"
-              className="h-4 w-4 rounded-full checked:bg-blue-600 hover:border-blue-600"
-              checked={todo.isCompleted}
-              onChange={() => handleToggleTodo(todo.id)}
-            />
-            <label htmlFor={`todo-${todo.id}`} className="ml-3">
-              <p className={todo.isCompleted ? "line-through text-gray-500" : undefined}>
-                {todo.text}
-              </p>
-            </label>
-          </div>
-        </div>
+      <div className="px-4 py-4 sm:px-6 flex items-center">
+        <input
+          id={`todo-${todo.id}`}
+          type="checkbox"
+          className="h-4 w-4 rounded-full checked:bg-blue-600 hover:border-blue-600"
+          checked={todo.isCompleted}
+          onChange={() => handleToggleTodo(todo.id)}
+        />
+        <label htmlFor={`todo-${todo.id}`} className="ml-5">
+          <p className={todo.isCompleted ? "line-through text-gray-500" : undefined}>
+            {todo.text}
+          </p>
+        </label>
       </div>
     </li>
   );
@@ -40,7 +36,7 @@ function TodoItem({ todo, handleToggleTodo }: TodoItemProps) {
 export default function TodoList({ todos, handleToggleTodo, handleAddTodo }: TodoListProps) {
   return (
     <ul className="mx-6">
-      <NewTodoForm handleAddTodo={handleAddTodo}/>
+      <NewTodoForm handleAddTodo={handleAddTodo} />
       {todos.map((todo) => (
         <TodoItem todo={todo} handleToggleTodo={handleToggleTodo} />
       ))}
