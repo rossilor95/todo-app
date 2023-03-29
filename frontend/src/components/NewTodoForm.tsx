75f170801c028fc2720adb4ee209cc728d82a088
rossilor95 <rossilor95@outlook.it>
2023-03-29 16:10:00 +0200

    feat(frontend): add styling to NewTodoForm

--- frontend/src/components/NewTodoForm.tsx
+++ frontend/src/components/NewTodoForm.tsx
@@ -1,4 +1,6 @@
-import { FormEvent, FormEventHandler, useState } from "react";
+import { FormEvent, useState } from "react";
+import { HiPlus } from "react-icons/hi2";
+
 
 interface TodoFormProps {
   handleAddTodo: (text: string) => void;
@@ -17,19 +19,19 @@ export default function TodoForm({ handleAddTodo }: TodoFormProps) {
   };
 
   return (
-    <li className="bg-white border-b border-gray-200 shadow hover:bg-blue-50 sm:rounded-md mb-4">
-      <div className="px-4 py-4 sm:px-6">
-        <div className="flex items-center justify-between">
-          <div className="flex items-center">
-            <form onSubmit={handleSubmitTodo}>
-              <input
-                type="text"
-                value={todoText}
-                onChange={e => setTodoText(e.target.value)}
-              />
-            </form>
-          </div>
-        </div>
+    <li className="bg-white border-b border-gray-200 shadow sm:rounded-md mb-4">
+      <div className="px-4 py-2 sm:px-5 flex items-center">
+        <HiPlus className="h-6 w-6 text-blue-600" />
+        <form onSubmit={handleSubmitTodo}>
+          <input
+            id="todo"
+            className="border-none flex-grow ml-0.5 placeholder-blue-600 focus:ring-transparent focus:placeholder-black"
+            type="text"
+            value={todoText}
+            placeholder="Add todo"
+            onChange={e => setTodoText(e.target.value)}
+          />
+        </form>
       </div>
     </li>
   );
