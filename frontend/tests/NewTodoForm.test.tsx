064ec6ca3f767c6feaf78f5c3802a40cbccadc84
rossilor95 <rossilor95@outlook.it>
2023-04-04 00:18:41 +0200

    test(frontend): add test for NewTodoForm

---
+++ frontend/tests/NewTodoForm.test.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { describe, expect, it, vitest } from "vitest";
+import NewTodoForm from "../src/components/NewTodoForm";
+
+describe("NewTodoForm", () => {
+  it("should render correctly", () => {
+    const handleAddTodo = vitest.fn();
+    render(<NewTodoForm handleAddTodo={handleAddTodo} />);
+    expect(screen.getByPlaceholderText("Add todo")).toBeInTheDocument();
+  });
+
+  it("should call handleAddTodo when submitting non-empty todo", async () => {
+    const user = userEvent.setup();
+    const handleAddTodo = vitest.fn();
+    render(<NewTodoForm handleAddTodo={handleAddTodo} />);
+    await user.click(screen.getByPlaceholderText("Add todo"));
+    await user.keyboard("Test[Enter]");
+    expect(handleAddTodo).toHaveBeenCalledWith("Test");
+  });
+
+  it("should not call handleAddTodo when submitting empty todo", async () => {
+    const user = userEvent.setup();
+    const handleAddTodo = vitest.fn();
+    render(<NewTodoForm handleAddTodo={handleAddTodo} />);
+    await user.click(screen.getByPlaceholderText("Add todo"));
+    await user.keyboard("[Backspace][Enter]");
+    expect(handleAddTodo).not.toHaveBeenCalled();
+  });
+});
