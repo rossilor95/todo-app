d0a5aaefd1536507f0341621fd9d318772dc2c20
rossilor95 <rossilor95@outlook.it>
2023-04-09 01:09:47 +0200

    feat(backend): add Todo controller

---
+++ backend/src/main/java/com/github/rossilor95/todo_app/todo/TodoController.java
@@ -0,0 +1,26 @@
+package com.github.rossilor95.todo_app.todo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/todos")
+public class TodoController {
+    private final TodoService todoService;
+
+    @Autowired
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Todo>> getAllTodos() {
+        return new ResponseEntity<>(todoService.findAll(), HttpStatus.OK);
+    }
+}
