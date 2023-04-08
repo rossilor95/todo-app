edef592717b086ecf4a36b24595e594165c4d968
rossilor95 <rossilor95@outlook.it>
2023-04-09 01:09:58 +0200

    feat(backend): add Category model

---
+++ backend/src/main/java/com/github/rossilor95/todo_app/category/Category.java
@@ -0,0 +1,54 @@
+package com.github.rossilor95.todo_app.category;
+
+import com.github.rossilor95.todo_app.todo.Todo;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+@Document("categories")
+public class Category {
+
+    @Id
+    private ObjectId id;
+
+    private String name;
+
+    @DocumentReference
+    private List<Todo> todoIds;
+
+    public Category(ObjectId id, String name, List<Todo> todoIds) {
+        this.id = id;
+        this.name = name;
+        this.todoIds = todoIds;
+    }
+
+    private Category() {
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Todo> getTodoIds() {
+        return todoIds;
+    }
+
+    public void setTodoIds(List<Todo> todoIds) {
+        this.todoIds = todoIds;
+    }
+}
