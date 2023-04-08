39e42f59b8a80accd4fe01865b8e2c299f638155
rossilor95 <rossilor95@outlook.it>
2023-04-09 01:09:29 +0200

    feat(backend): add Todo repository

---
+++ backend/src/main/java/com/github/rossilor95/todo_app/todo/TodoRepository.java
@@ -0,0 +1,9 @@
+package com.github.rossilor95.todo_app.todo;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends MongoRepository<Todo, ObjectId> {
+}
