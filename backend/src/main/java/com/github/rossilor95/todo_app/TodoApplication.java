a397f68b061586eb9c1f122761365c828ebc56b4
rossilor95 <rossilor95@outlook.it>
2023-04-11 23:12:43 +0200

    refactor(be): remove unused import

--- backend/src/main/java/com/github/rossilor95/todo_app/TodoApplication.java
+++ backend/src/main/java/com/github/rossilor95/todo_app/TodoApplication.java
@@ -3,7 +3,6 @@ package com.github.rossilor95.todo_app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.lang.NonNullApi;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
