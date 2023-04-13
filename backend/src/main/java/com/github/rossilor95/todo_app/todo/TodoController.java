e7adae1711715388307a277c56e85849be59e413
rossilor95 <rossilor95@outlook.it>
2023-04-13 19:44:54 +0200

    feat(be): add logging and basic error handling to TodoController

--- backend/src/main/java/com/github/rossilor95/todo_app/todo/TodoController.java
+++ backend/src/main/java/com/github/rossilor95/todo_app/todo/TodoController.java
@@ -1,6 +1,9 @@
 package com.github.rossilor95.todo_app.todo;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +19,7 @@ import java.util.List;
 @RequestMapping("/api/v1/todos")
 public class TodoController {
     private final TodoRepository todoRepository;
+    private final Logger logger = LoggerFactory.getLogger(TodoController.class);
 
     @Autowired
     public TodoController(TodoRepository todoRepository) {
@@ -24,16 +28,29 @@ public class TodoController {
 
     @GetMapping
     public ResponseEntity<List<Todo>> findAll() {
-        return ResponseEntity.ok(todoRepository.findAll());
+        try {
+            List<Todo> todos = todoRepository.findAll();
+            logger.info("Found {} todos", todos.size());
+            return ResponseEntity.ok(todos);
+        } catch (Exception e) {
+            logger.error("Error occurred while finding todos: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     @PostMapping
     public ResponseEntity<Todo> save(@RequestBody Todo todo) {
-        Todo savedTodo = todoRepository.save(todo);
-        URI uriOfTodo = ServletUriComponentsBuilder.fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(savedTodo.id())
-                .toUri();
-        return ResponseEntity.created(uriOfTodo).build();
+        try {
+            Todo savedTodo = todoRepository.save(todo);
+            URI uriOfTodo = ServletUriComponentsBuilder.fromCurrentRequest()
+                    .path("/{id}")
+                    .buildAndExpand(savedTodo.id())
+                    .toUri();
+            logger.info("New Todo created with id {}", savedTodo.id());
+            return ResponseEntity.created(uriOfTodo).build();
+        } catch (Exception e) {
+            logger.error("Error occurred while saving Todo: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
