aafc869075bee0ac6be6d7f999ad59b533cb4da4
rossilor95 <rossilor95@outlook.it>
2023-04-13 19:33:59 +0200

    feat(be): implement post new todo in TodoController

--- backend/src/main/java/com/github/rossilor95/todo_app/todo/TodoController.java
+++ backend/src/main/java/com/github/rossilor95/todo_app/todo/TodoController.java
@@ -3,9 +3,13 @@ package com.github.rossilor95.todo_app.todo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -14,13 +18,22 @@ public class TodoController {
     private final TodoRepository todoRepository;
 
     @Autowired
-    public
-    TodoController(TodoRepository todoRepository) {
+    public TodoController(TodoRepository todoRepository) {
         this.todoRepository = todoRepository;
     }
 
     @GetMapping
     public ResponseEntity<List<Todo>> findAll() {
         return ResponseEntity.ok(todoRepository.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<Todo> save(@RequestBody Todo todo) {
+        Todo savedTodo = todoRepository.save(todo);
+        URI uriOfTodo = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedTodo.id())
+                .toUri();
+        return ResponseEntity.created(uriOfTodo).build();
     }
 }
